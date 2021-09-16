ee2663b5eae923ff3430c781464c24cf1e15aa1a
chenxingchen <xingchen.chen@ziel.cn>
2021-09-16 17:40:50 +0800

    fix: 1.优化样式,图片加载速度  2.修复评论数量显示错误任务 3.修复粉丝列表无法翻页问题

--- src/pages/followers/[user_id].tsx
+++ src/pages/followers/[user_id].tsx
@@ -6,10 +6,14 @@ import { followUser, getFollowers, getProfileInfo, ProfileType } from "@/apis/pr
 import Tabs from "#/lib/Tabs";
 import TabPanel from "#/lib/Tabs/TabPanel";
 import { useEffect, useMemo, useState } from "react";
-import Button from "#/lib/Button";
 import produce from "immer";
 import { useAppSelector } from "@/app/hook";
 import { useRouter } from "next/router";
+import FollowedIcon from "@/assets/icons/Followed_profile.svg";
+import UnFollowedIcon from "@/assets/icons/unFollowed_profile.svg";
+import Loading from "#/lib/Loading";
+import Empty from "#/lib/Empty";
+import Image from "#/lib/Image";
 
 interface FollowersType {
   profileInfo: ProfileType;
@@ -22,9 +26,6 @@ interface FollowersType {
 const Container = styled.div`
   width: 100%;
   height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
 `;
 const ListItem = styled.div`
   display: flex;
@@ -36,10 +37,13 @@ const ListItem = styled.div`
 const UserInfo = styled.div`
   display: flex;
   align-items: center;
+  padding-right: 10px;
 `;
-const Avatar = styled.img`
+const Avatar = styled(Image)`
   height: 48px;
   width: 48px;
+  min-width: 48px;
+  max-width: 48px;
   border-radius: 50%;
 `;
 const Name = styled.div`
@@ -48,17 +52,12 @@ const Name = styled.div`
   font-size: 16px;
   line-height: 19px;
 `;
-const FollowBtn = styled(Button)`
-  font-weight: 500;
-  font-size: 14px;
-  line-height: 16px;
-  height: 30px;
-`;
 const TabsStyle = styled(Tabs)`
   display: flex;
   justify-content: space-between;
   margin: 0 50px;
   width: calc(100% - 100px);
+  z-index: 1;
 `;
 const TabPanelStyle = styled(TabPanel)``;
 
@@ -69,28 +68,33 @@ const Followers: NextPage<FollowersType> = ({ profileInfo }) => {
   const [type, setType] = useState<"1" | "2">(router.query.tabIndex === "1" ? "1" : "2");
   const [profile, setProfile] = useState<ProfileType>(profileInfo);
   const [followers, setFollowers] = useState<FollowersType[]>([]);
-  const { loaderRef, page, setLoading, setHasMore, hasMore } = useInfiniteScroll<HTMLDivElement>({
-    hasMore: false,
-    initialPage: 0,
-  });
+  const { loaderRef, page, setLoading, setHasMore, hasMore, setPage, loading } =
+    useInfiniteScroll<HTMLDivElement>({
+      hasMore: false,
+      initialPage: 1,
+    });
   const followType = useMemo(() => (type == "1" ? "followers" : "following"), [type]);
   const handleChange = (key: string) => {
     if (key === type) return;
     setType(key as "1" | "2");
     setFollowers([]);
+    setPage(1);
+    setLoading(true);
   };
   useEffect(() => {
-    setLoading(true);
-    getFollowers({ userId: profile.id, type: followType, page })
-      .then((response) => {
-        const list = response.data.result.data;
-        const hasMore = Boolean(response.data.result.hasmore);
-        setHasMore(hasMore);
-        setFollowers((prev) => [...prev, ...list]);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
+    (() => {
+      setLoading(true);
+      getFollowers({ userId: profile.id, type: followType, page })
+        .then((response) => {
+          const list = response.data.result.data;
+          const hasMore = Boolean(response.data.result.hasmore);
+          setHasMore(hasMore);
+          setFollowers((prev) => [...prev, ...list]);
+        })
+        .finally(() => {
+          setLoading(false);
+        });
+    })();
   }, [followType, page, profile.id, setHasMore, setLoading]);
 
   const handleFollow = withLogin<FollowersType>(async (follower) => {
@@ -122,53 +126,67 @@ const Followers: NextPage<FollowersType> = ({ profileInfo }) => {
   return (
     <>
       <Container>
-        <Header>{profile.nickname}</Header>
+        <Header>Follow</Header>
         <TabsStyle activeKey={type} onChange={handleChange}>
-          <TabPanelStyle indexKey="1" tab={`Followers ${digitalScale(profile.followerNum)}`}>
-            {followers.map((follower) => (
-              <ListItem key={follower.id}>
-                <UserInfo>
-                  <Avatar src={follower.avatar} />
-                  <Name>{follower.nickname}</Name>
-                </UserInfo>
-                {user.uid === follower.id ? (
-                  ""
-                ) : (
-                  <FollowBtn
-                    onClick={() => {
-                      handleFollow(follower);
-                    }}
-                    color={follower.isFollow ? "default" : "primary"}
-                  >
-                    {follower.isFollow ? "unFollow" : "Follow"}
-                  </FollowBtn>
-                )}
-              </ListItem>
-            ))}
-            {hasMore ? <div ref={loaderRef}>loading...</div> : null}
+          <TabPanelStyle
+            indexKey="1"
+            tab={`Followers ${digitalScale(profile.followerNum)}`}
+            forceRender
+          >
+            {followers.length === 0 && !loading ? (
+              <Empty description="No Followers" />
+            ) : (
+              followers.map((follower) => (
+                <ListItem key={follower.id}>
+                  <UserInfo>
+                    <Avatar blur width={48} height={48} src={follower.avatar} />
+                    <Name>{follower.nickname}</Name>
+                  </UserInfo>
+                  {user.uid === follower.id ? (
+                    ""
+                  ) : (
+                    <div
+                      onClick={() => {
+                        handleFollow(follower);
+                      }}
+                    >
+                      {follower.isFollow ? <FollowedIcon /> : <UnFollowedIcon />}
+                    </div>
+                  )}
+                </ListItem>
+              ))
+            )}
+            {hasMore ? <Loading ref={loaderRef} /> : null}
           </TabPanelStyle>
-          <TabPanelStyle indexKey="2" tab={`Following ${digitalScale(profile.followingNum)}`}>
-            {followers.map((follower) => (
-              <ListItem key={follower.id}>
-                <UserInfo>
-                  <Avatar src={follower.avatar} />
-                  <Name>{follower.nickname}</Name>
-                </UserInfo>
-                {user.uid === follower.id ? (
-                  ""
-                ) : (
-                  <FollowBtn
-                    onClick={() => {
-                      handleFollow(follower);
-                    }}
-                    color={follower.isFollow ? "secondary" : "primary"}
-                  >
-                    {follower.isFollow ? "unFollow" : "Follow"}
-                  </FollowBtn>
-                )}
-              </ListItem>
-            ))}
-            {hasMore ? <div ref={loaderRef}>loading...</div> : null}
+          <TabPanelStyle
+            forceRender
+            indexKey="2"
+            tab={`Following ${digitalScale(profile.followingNum)}`}
+          >
+            {followers.length === 0 && !loading ? (
+              <Empty description="No Followers" />
+            ) : (
+              followers.map((follower) => (
+                <ListItem key={follower.id}>
+                  <UserInfo>
+                    <Avatar blur width={48} height={48} src={follower.avatar} />
+                    <Name>{follower.nickname}</Name>
+                  </UserInfo>
+                  {user.uid === follower.id ? (
+                    ""
+                  ) : (
+                    <div
+                      onClick={() => {
+                        handleFollow(follower);
+                      }}
+                    >
+                      {follower.isFollow ? <FollowedIcon /> : <UnFollowedIcon />}
+                    </div>
+                  )}
+                </ListItem>
+              ))
+            )}
+            {hasMore ? <Loading ref={loaderRef} /> : null}
           </TabPanelStyle>
         </TabsStyle>
       </Container>
